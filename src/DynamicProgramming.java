608b6d72fb2202b840163fef36eb95e0084edc72
ankith.bajpai@gmail.com <ankit_bajpai@homedepot.com>
2018-08-02 23:53:57 -0500

    Some more code

--- src/DynamicProgramming.java
+++ src/DynamicProgramming.java
@@ -4,7 +4,37 @@ import static java.util.Arrays.*;
 
 public class DynamicProgramming {
     public static void main(String[] args) {
+        longestValidParentheses("(()");
 
+    }
+    public static int longestValidParentheses(String s1) {
+        Stack<Integer> s = new Stack<>();
+        boolean[] b = new boolean[s1.length()];
+        for(int i =0; i < s1.length(); i ++){
+            char c = s1.charAt(i);
+            if(c=='('){
+                s.push(i);
+            }
+            else {
+                if(!s.isEmpty()){
+                    b[i]=true;
+                    b[s.pop()]=true;
+                }
+            }
+
+        }
+        int count=0;
+        int res=0;
+        for(int i =0; i < b.length; i ++){
+            if(b[i]){
+                count++;
+            }
+            else {
+                res = Math.max(count,res);
+                count=0;
+            }
+        }
+        return res;
     }
 
     public int minimumTotal(List<List<Integer>> t) {
